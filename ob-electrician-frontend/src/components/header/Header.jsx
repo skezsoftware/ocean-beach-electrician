8e2e7be7c5e8e595fd40589ce0e7c0db7f7b4c27
Daniel Vasquez <dwvasquez97@gmail.com>
2024-12-01 19:26:33 -0800

    added logo, nav bar adjsutments and more

--- ob-electrician-frontend/src/components/header/Header.jsx
+++ ob-electrician-frontend/src/components/header/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.png";
 import "./header.css";
 
 const Header = () => {
   return (
     <nav className="nav-bar">
-      <div className="logo">OB Electrician Logo</div>
+      <img src={Logo} alt="logo" className="logo" />
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/services">Services</Link>
